a3c0e9160b6254e737b3cc46140095cf42886251
Nog <141933115+EllPrivico@users.noreply.github.com>
2026-06-25 18:15:04 -0500

    Update survival-plus.js

--- survival-plus.js
+++ survival-plus.js
@@ -1,6 +1,10 @@
 Object.entries(elementWorth).forEach(([key, value]) => {
     if (!(elements[key].desc)){
-        elements[key].desc=""
+        elements[key].desc="";
     }
-    elements[key].desc+="\n Worth $"+value;
+    if (value>0){
+        elements[key].desc+="\nWorth $"+value;
+    } else if (value===-1){
+        elements[key].desc+="\nDisappears if you try to sell it";
+    }
 });
