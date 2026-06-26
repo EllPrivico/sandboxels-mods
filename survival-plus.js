6edcfd3b9b6937125db02eb8985d811190580891
Nog <141933115+EllPrivico@users.noreply.github.com>
2026-06-26 13:23:42 -0500

    Update survival-plus.js

--- survival-plus.js
+++ survival-plus.js
@@ -36,3 +36,24 @@ Object.entries(elements).forEach(([key, value]) => {
         elements[key].desc+="Worth $1";
     }
 });
+
+createCategoryDiv("multibuy");
+var multibuyDiv = document.getElementById("category-multibuy");
+multibuyDiv.style.display = "none";
+
+function mbuyupdate(){
+    multibuyDiv.innerHTML = "";
+    multibuyDiv.insertAdjacentHTML("beforeend",`<p>You have $<span id="coinCount">${settings.survival.gold_coin||0}</span></p>`);
+    for (var element in survivalShop){
+        var line = document.createElement("p");
+        var price = survivalShop[element];
+        var name = element;
+        var amount = 1;
+        if (element.indexOf("*") !== -1) { amount = parseInt(element.split("*")[1]); name = element.split("*")[0]; }
+        var elemname = name;
+        alert(amount);
+        name = (elements[elemname].name||name).replace(/_/g, " ").replace(".","   ").replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();}).replace("   ",".").replace(/ /g, "");
+    }
+}
+
+mbuyupdate();
