5213b2e7f6216961f531417e83b0d4eb59a02bd3
Nog <141933115+EllPrivico@users.noreply.github.com>
2026-06-26 14:00:26 -0500

    Update survival-plus.js

--- survival-plus.js
+++ survival-plus.js
@@ -52,12 +52,12 @@ function mbuyupdate(){
         if (element.indexOf("*") !== -1) { amount = parseInt(element.split("*")[1]); name = element.split("*")[0]; }
         var elemname = name;
         name = (elements[elemname].name||name).replace(/_/g, " ").replace(".","   ").replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();}).replace("   ",".").replace(/ /g, "");
-        var temp = ``
-        if (amount!=1){
-            temp+=`${amount} pack of `
-        }
-        temp += `$(element) for $${price}`
-        line.insertAdjacentHTML("beforeend", temp);
+        //var temp = ``
+        //if (amount!=1){
+        //    temp+=`${amount} pack of `
+        //}
+        //temp += `$(element) for $${price}`
+        //line.insertAdjacentHTML("beforeend", temp);
     }
 }
 
