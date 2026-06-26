bd2371ba9426938b4bd704df1c59664cbd102070
Nog <141933115+EllPrivico@users.noreply.github.com>
2026-06-26 15:52:51 -0500

    Update survival-plus.js

--- survival-plus.js
+++ survival-plus.js
@@ -11,10 +11,10 @@ elements.irradiate = {
     category: "tools",
     tool: function(pixel) {
         if(Object.keys(elements[pixel.element].reactions).includes("radiation")){
-            alert(Object.keys(elements[pixel.element].reactions))
+            alert(Object.keys(elements[pixel.element].reactions.radiation))
         }
         if(Object.keys(elements.radiation.reactions).includes(pixel.element)){
-            alert(Object.keys((elements.radiation.reactions[pixel.element])))
+            alert((elements.radiation.reactions[pixel.element]).elem2)
         }
     }
 };
