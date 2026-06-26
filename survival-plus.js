0d2c17a016bd3f191f2b5aa9e2557f7d59975203
Nog <141933115+EllPrivico@users.noreply.github.com>
2026-06-26 16:03:14 -0500

    Update survival-plus.js

--- survival-plus.js
+++ survival-plus.js
@@ -11,7 +11,7 @@ elements.irradiate = {
     category: "tools",
     tool: function(pixel) {
         if(Object.keys(elements[pixel.element].reactions).includes("radiation")){
-            alert(Object.keys(elements[pixel.element].reactions.radiation))
+            alert(elements[pixel.element].reactions.radiation.elem1)
         }
         if(Object.keys(elements.radiation.reactions).includes(pixel.element)){
             alert((elements.radiation.reactions[pixel.element]).elem2)
